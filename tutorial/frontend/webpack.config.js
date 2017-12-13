7dcb75bd9f0748e5c3bdc353dcf1fbd17fcdac3d
Takuhiro Fujii <fujii_ta@camobile.com>
2017-12-13 20:42:45 +0900

    階層整理

--- tutorial/frontend/webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    entry: './js/main.js',
-    output: {
-        path: __dirname,
-        filename: 'bundle.min.js'
-    }
-};
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: __dirname + '/tutorial/frontend/js/main.js',
+    output: {
+        path: __dirname + '/tutorial/frontend/',
+        filename: 'bundle.min.js'
+    }
+};
